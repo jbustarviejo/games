9e77f6ab22faa9a0626fe23c7d0292cc4a0e8081
Josué Bustarviejo <jbustarviejo@MacBook-de-jbustarviejogmailcom.local>
2015-06-26 20:52:33 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html
+	PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html>
+
+<head>
+	<title>Games</title>
+</head>
+
+<body>
+
+	<canvas id="myCanvas" width="200" height="100" style="border:1px solid #000000;">
+		Parece que tu navegador no es compatible :(
+	</canvas> 
+
+<div>
+<h2>Dudas</h2>
+	<ul>
+		<li></li>
+	</ul>
+<h2>Todo</h2>
+	<ul>
+		<li></li>
+	</ul>
+</div>
+
+	<script type="text/javascript">
+	var c = document.getElementById("myCanvas");
+	var ctx = c.getContext("2d");
+	ctx.fillStyle = "#FF0000";
+	ctx.fillRect(0,0,150,75);
+	</script>
+</body>
+</html>
