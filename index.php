9303b64098505d192ed366498fac50366a766cc3
Josué Bustarviejo <jbustarviejo@MacBook-de-jbustarviejogmailcom.local>
2015-07-04 08:42:11 +0200

    First aproach

--- index.php
+++ index.php
@@ -15,23 +15,20 @@
                 </div>
                 <button class="play-button" onclick="sticksGame.startSticksGame();">Jugar</button>
             </div>
-            <div id="main-scren-game" class="screen" style="z-index: 8; background-color: rgb(117, 117, 219);">
+            <div id="main-screen-game-sticks" class="screen" style="z-index: 8; background-color: rgb(117, 117, 219);">
                 <img id="close-hand" ondragstart="return false;" src="/images/larguest-stick/close-hand.png" style=""/>
-                <img class="stick" ondragstart="return false;" number="1" src="/images/larguest-stick/stick.png" onmouseover="sticksGame.createsoundbite('/audio/click.ogg', '/audio/click.mp3').play();" style="left:43%"/>
-                <img class="stick" ondragstart="return false;" number="2" src="/images/larguest-stick/stick.png" onmouseover="sticksGame.createsoundbite('/audio/click.ogg', '/audio/click.mp3').play();"/>
-                <img class="stick" ondragstart="return false;" number="3" src="/images/larguest-stick/stick.png" onmouseover="sticksGame.createsoundbite('/audio/click.ogg', '/audio/click.mp3').play();"  style="left:57%"/>
-                <img id="open-hand" ondragstart="return false;" src="/images/larguest-stick/open-hand.png" style="display:none;bottom:10%;"/>
+                <img id="open-hand" ondragstart="return false;" src="/images/larguest-stick/open-hand.png" style="display:none;bottom:2%;opacity: 1;"/>
             </div>
             <div id="win-screen" class="screen menu-screen" style="display:none;">
                 <div style="color: #fff; margin-top: 22%;">
-                    <h2>Enhorabuena ¡Has ganado!</h2>
-                    <p>Has conseguido X puntos</p>
+                    <h2 class="h2-title">Enhorabuena ¡Has ganado!</h2>
+                    <p class="screen-msg">Has conseguido X puntos</p>
                 </div>
             </div>
             <div id="lose-screen" class="screen menu-screen" style="display:none;">
                 <div style="color: #fff; margin-top: 22%;">
-                    <h2>Vaya... No has ganado...</h2>
-                    <p>Has perdido Y puntos</p>
+                    <h2 class="h2-title">Vaya... No has ganado...</h2>
+                    <p class="screen-msg">Has perdido Y puntos</p>
                 </div>
             </div>
         </div>
@@ -54,19 +51,27 @@
 
         <script type="text/javascript">
             window.onload = function () {
-                sticksGame.init();
+                sticksGame.init(Math.floor(Math.random() * 3) + 3);
             };
             var sticksGame = {
-                init: function () {
+                init: function (sticksNumber) {
                     var self = this;
-                    var sticks = document.getElementsByClassName("stick");
-                    self.winner_stick = Math.floor(Math.random() * 3) + 1;
-                    self.sticksNumber = 3;
-                    for (var i = 0; i < sticks.length; i++) {
-                        //Bind sticks onclicks
-                        sticks[i].onclick = function () {
-                            self.chooseStick(this.getAttribute("number"));
-                        };
+                    self.sticksNumber = sticksNumber;
+                    self.winner_stick = Math.floor(Math.random() * sticksNumber) + 1;
+                    var sticksContainer = document.getElementById('main-screen-game-sticks');
+                    console.log("sticksNumber: " + sticksNumber);
+                    var availableWidth = 20;
+                    var leftInc = Math.round(availableWidth * 100 / sticksNumber) / 100;
+                    var leftOffset = 39;
+                    for (var i = 0; i < sticksNumber; i++) {
+                        if (i === self.winner_stick - 1) {
+                            var height = 70;
+                        } else {
+                            var height = Math.floor(Math.random() * 12) + 38;
+                        }
+                        sticksContainer.innerHTML += self.createsoundbite('/audio/click.ogg', '/audio/click.mp3', (i + 1)).outerHTML;
+                        sticksContainer.innerHTML += '<img class="stick" onclick="sticksGame.chooseStick(' + (i + 1) + ')" ondragstart="return false;" number="' + i + '" src="/images/larguest-stick/straw' + (Math.floor(Math.random() * 7) + 1) + '.jpg" onmouseover="document.getElementById(\'stickAudio' + (i + 1) + '\').play();" style="left:' + leftOffset + '%; height:' + height + '%">';
+                        leftOffset += leftInc;
                     }
                     //Bind on resize
                     window.onresize = function () {
@@ -78,42 +83,6 @@
                     this.start_decission = new Date().getTime();
                     document.getElementById('instructions-screen').style.display = 'none';
                 },
-                createsoundbite: function (sound) {
-                    var html5_audiotypes = {//define list of audio file extensions and their associated audio types. Add to it if your specified audio file isn't on this list:
-                        "mp3": "audio/mpeg",
-                        "mp4": "audio/mp4",
-                        "ogg": "audio/ogg",
-                        "wav": "audio/wav"
-                    };
-                    var html5audio = document.createElement('audio');
-                    if (html5audio.canPlayType) { //check support for HTML5 audio
-                        for (var i = 0; i < arguments.length; i++) {
-                            var sourceel = document.createElement('source');
-                            sourceel.setAttribute('src', arguments[i]);
-                            if (arguments[i].match(/\.(\w+)$/i)) {
-                                sourceel.setAttribute('type', html5_audiotypes[RegExp.$1]);
-                            }
-                            html5audio.appendChild(sourceel);
-                        }
-                        html5audio.load();
-                        html5audio.playclip = function () {
-                            html5audio.pause();
-                            html5audio.currentTime = 0;
-                            html5audio.play();
-                        };
-                        return html5audio;
-                    } else {
-                        return {
-                            playclip: function () {
-                                throw new Error("Your browser doesn't support HTML5 audio unfortunately");
-                            }
-                        };
-                    }
-                },
-                //Returns Body tag width displayed on window
-                getBodyWidth: function () {
-                    return document.getElementById("body").offsetWidth;
-                },
                 animateDown: function (obj, to, choosen) {
                     var self = this;
                     var bottomInit = obj.style.bottom;
@@ -122,7 +91,8 @@
                         self.finish(choosen);
                         return;
                     }
-                    obj.style.bottom = (bottomInit - 0.3) + "%";
+                    obj.style.bottom = (bottomInit - 0.5) + "%";
+                    obj.style.opacity -= 0.05;
                     setTimeout(function () {
                         self.animateDown(obj, to, choosen);
                     }, 50);
@@ -139,7 +109,7 @@
                     var sticks = document.getElementsByClassName("stick");
                     for (var i = 0; i < sticks.length; i++) {
                         //Bind sticks onclicks
-                        if (i == (choosen - 1)) {
+                        if (i === (choosen - 1)) {
                             sticks[i].setAttribute("class", sticks[i].className + " selected");
                         } else {
                             sticks[i].setAttribute("class", sticks[i].className + " no-selected");
@@ -148,21 +118,48 @@
                         sticks[i].onclick = "";
                     }
                     setTimeout(function () {
-                        self.animateDown(document.getElementById("open-hand"), -8, choosen);
+                        self.animateDown(document.getElementById("open-hand"), -20, choosen);
                     }, 1200);
                 },
                 finish: function (choosen) {
-                    if (this.winner_stick.toString() === choosen) {
+                    if (this.winner_stick === choosen) {
                         document.getElementById("win-screen").style.display = "block";
                     } else {
                         document.getElementById("lose-screen").style.display = "block";
                     }
                 },
-                //Function that controls the rigth width and height
-                resized: function () {
-                    var size = Math.round(this.getBodyWidth() * 0.75);
-                    document.getElementById("gameContainer").style.width = size + "px";
-                    document.getElementById("gameContainer").style.height = Math.round(size * 0.56) + "px";
+                createsoundbite: function (sound) {
+                    var html5_audiotypes = {//define list of audio file extensions and their associated audio types. Add to it if your specified audio file isn't on this list:
+                        "mp3": "audio/mpeg",
+                        //"mp4": "audio/mp4",
+                        "ogg": "audio/ogg"
+                                //"wav": "audio/wav"
+                    };
+                    var html5audio = document.createElement('audio');
+                    html5audio.setAttribute("id", "stickAudio" + (arguments[arguments.length - 1]));
+                    if (html5audio.canPlayType) { //check support for HTML5 audio
+                        for (var i = 0; i < arguments.length - 1; i++) {
+                            var sourceel = document.createElement('source');
+                            sourceel.setAttribute('src', arguments[i]);
+                            if (arguments[i].match(/\.(\w+)$/i)) {
+                                sourceel.setAttribute('type', html5_audiotypes[RegExp.$1]);
+                            }
+                            html5audio.appendChild(sourceel);
+                        }
+                        html5audio.load();
+                        /*html5audio.playclip = function () {
+                         html5audio.pause();
+                         html5audio.currentTime = 0;
+                         html5audio.play();
+                         };*/
+                        return html5audio;
+                    } else {
+                        return {
+                            playclip: function () {
+                                throw new Error("Your browser doesn't support HTML5 audio unfortunately");
+                            }
+                        };
+                    }
                 },
                 sendDataToServer: function (time, winner, selected, sticksNumber) {
                     var xhr = new XMLHttpRequest();
@@ -178,8 +175,18 @@
                         }
                     };
                     xhr.send(encodeURI('time=' + time) + "&" + encodeURI('winner=' + winner) + "&" + encodeURI('selected=' + selected) + "&" + encodeURI('sticksNumber=' + sticksNumber));
+                },
+                //Function that controls the rigth width and height
+                resized: function () {
+                    var size = Math.round(this.getBodyWidth() * 0.75);
+                    document.getElementById("gameContainer").style.width = size + "px";
+                    document.getElementById("gameContainer").style.height = Math.round(size * 0.56) + "px";
+                },
+                //Returns Body tag width displayed on window
+                getBodyWidth: function () {
+                    return document.getElementById("body").offsetWidth;
                 }
-            }
+            };
         </script>
     </body>
 </html>
